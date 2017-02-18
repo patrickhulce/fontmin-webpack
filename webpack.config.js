ded5580962428ad193b2b3eb396e2a24325169cc
Patrick Hulce <phulce@google.com>
2017-02-17 16:14:55 -0800

    basic ttf working

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
   output: {filename: 'out.js', path: `${__dirname}/dist`},
   module: {
     rules: [
-      {test: /\.(woff|woff2)(\?v=.+)?$/, use: ['url-loader']},
+      {test: /\.(woff|woff2)(\?v=.+)?$/, use: ['file-loader']},
       {test: /\.(svg|ttf|eot)(\?v=.+)?$/, use: ['file-loader']},
       {test: /\.css$/, use: ExtractTextPlugin.extract({use: 'css-loader'}), include: __dirname},
     ],
