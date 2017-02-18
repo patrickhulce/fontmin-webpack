48eb86d010c705c636f31f3793cd30cc4acbf441
Patrick Hulce <patrick.hulce@gmail.com>
2017-02-18 12:25:07 -0800

    fix: cleanup code and add tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,7 +1,77 @@
-defineTest('index.js', function (app) {
-  describe('#app', function () {
-    it('should work', function () {
-      // something.should.equal(2);
-    });
-  });
-});
+const fs = require('fs')
+const path = require('path')
+const _ = require('lodash')
+const expect = require('chai').expect
+const webpack = require('webpack')
+const Plugin = require('../lib')
+
+describe('FontminPlugin', function () {
+  let fontStats
+  const baseConfig = {
+    entry: `${__dirname}/fixtures/entry.js`,
+    output: {filename: 'out.js', path: `${__dirname}/dist`},
+    module: {
+      rules: [
+        {test: /\.(woff|woff2)(\?v=.+)?$/, use: ['file-loader']},
+        {test: /\.(svg|ttf|eot)(\?v=.+)?$/, use: ['file-loader']},
+        {test: /\.css$/, use: ['style-loader', 'css-loader'], include: __dirname},
+      ],
+    },
+  }
+
+  function collectFontStats(stats) {
+    return _.keys(stats.compilation.assets)
+      .map(filename => {
+        return {
+          filename,
+          extension: path.extname(filename),
+          stats: fs.statSync(`${__dirname}/dist/${filename}`),
+        }
+      })
+      .filter(item => item.extension !== '.js')
+  }
+
+  function testWithConfig(config, done) {
+    webpack(config, (err, stats) => {
+      if (err) {
+        done(err)
+      } else {
+        fontStats = collectFontStats(stats)
+        done()
+      }
+    })
+  }
+
+  describe('FontAwesome micro', function () {
+    before(done => {
+      const plugin = new Plugin({glyphs: '\uf0c7'})
+      const config = _.cloneDeep(baseConfig)
+      testWithConfig(_.assign(config, {plugins: [plugin]}), done)
+    })
+
+    it('should minify eot', () => {
+      const eot = _.find(fontStats, {extension: '.eot'})
+      expect(eot).to.have.deep.property('stats.size').greaterThan(500).lessThan(2400)
+    })
+
+    it('should minify svg', () => {
+      const svg = _.find(fontStats, {extension: '.svg'})
+      expect(svg).to.have.deep.property('stats.size').greaterThan(500).lessThan(2000)
+    })
+
+    it('should minify tff', () => {
+      const ttf = _.find(fontStats, {extension: '.ttf'})
+      expect(ttf).to.have.deep.property('stats.size').greaterThan(500).lessThan(2200)
+    })
+
+    it('should minify woff', () => {
+      const woff = _.find(fontStats, {extension: '.woff'})
+      expect(woff).to.have.deep.property('stats.size').greaterThan(500).lessThan(2300)
+    })
+
+    it('should minify woff2', () => {
+      const woff2 = _.find(fontStats, {extension: '.woff2'})
+      expect(woff2).to.have.deep.property('stats.size').greaterThan(500).lessThan(1000)
+    })
+  })
+})
